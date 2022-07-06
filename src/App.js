1f211ed8c9754e94205528254be06277080f00a1
OlehMartyniak <97449514+OlehMartyniak@users.noreply.github.com>
2022-07-06 22:25:45 +0300

    react_lecture_3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import {useForm} from "react-hook-form";
+
+let usersList = [
+    {id: 1,name:'Mary'},
+    {id: 2,name:'Lisbeth'},
+    {id: 3,name:'Vinona'}
+];
 
 const App = () => {
+    
+
+let {register,handleSubmit,formState:{errors}} = useForm();
+
+let submit = (obj)=> {
+    console.log(obj)
+    
+
+}
+
     return (
         <div>
-          
+
+            <form onSubmit={handleSubmit(submit)}>
+                <input type="text" {...register('title',{required:true})}/>
+                <input type="text" {...register('body')}/>
+
+                <select {...register('UserId')}>
+                    {usersList.map(value => <option value={value.id} key={value.id}>{value.name}</option>)}
+                </select>
+
+                <button>save</button>
+                {
+                    errors.title && <p>field is required</p>
+                }
+            </form>
+
         </div>
     );
 };
