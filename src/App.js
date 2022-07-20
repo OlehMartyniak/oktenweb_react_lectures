bde119767af63b3f24f21d1e135b395fb05ed9a7
OlehMartyniak <97449514+OlehMartyniak@users.noreply.github.com>
2022-07-20 11:07:32 +0300

    redux_p1 (createStore,useSelector,useDispatch,отримання і передавання інформації в store)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
 
 const App = () => {
 
+    let state = useSelector(state => state);
+    console.log(state);
+    let dispatch = useDispatch();
+
     return (
         <div>
-          
+
+            {state}
+
+            <button onClick={()=> {
+                dispatch({type:'INC',payload:1})
+            }}>INCrement</button>
+
+            <button onClick={()=> {
+                dispatch({type:'DEC',payload:1})
+            }}>DECrement</button>
+
+            <button onClick={()=> {
+                dispatch({type:'RES'})
+            }}>RES</button>
+
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+let store = createStore((state=0,action)=>{
+    switch (action.type) {
+
+        case 'INC':
+            let newStateValue = state + action.payload;
+            return newStateValue;
+
+        case 'DEC':
+            return state - action.payload;
+
+        case 'RES':
+            return 0
+
+        default:
+            return state;
+    }
+})
+
+
 root.render(
   <React.StrictMode>
-    <App />
+
+      <Provider store={store}>
+          <App />
+      </Provider>
+
   </React.StrictMode>
 );
